48714cac6d0fa5c6194e8867c470dda59bb9c7ea
slb <Christian.Schellenberger@mbbm-vas.com>
2025-09-30 08:34:34 +0200

    breaking change => v1

--- gamma.go
+++ gamma.go
@@ -1,5 +1,5 @@
 package mvslibgamma
 
-func Gamma() string {
-	return "This is Gamma v0.3.0 speaking."
+func Gamma(message string) string {
+	return "This is Gamma v0.3.0 speaking: " + message
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/mbbm-slb/mvs_lib_gamma
+module github.com/mbbm-slb/mvs_lib_gamma/v1
 
 go 1.25.1
