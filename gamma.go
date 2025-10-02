2c35b0a1009d206fc80a3a9b92c8dce5bf4e5d49
slb <Christian.Schellenberger@mbbm-vas.com>
2025-10-02 14:15:54 +0200

    changes for Version vs.0.2

--- gamma.go
+++ gamma.go
@@ -1,5 +1,5 @@
 package mvslibgamma
 
 func Gamma(message, message2 string) string {
-	return "This is Gamma v2.0.1 speaking: " + message + " and " + message2
+	return "This is Gamma v2.0.2 speaking: " + message + " and " + message2
 }
